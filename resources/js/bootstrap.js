d98e4d7d4fd0a63d4f0ab42a9eaa0a8f5385153d
supergrecko <business@supergrecko.com>
2019-02-13 19:55:24 +0100

    Chnages

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,7 +7,6 @@ window._ = require('lodash');
  * code may be modified to fit the specific needs of your application.
  */
 
-/*
 try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
@@ -15,7 +14,6 @@ try {
     require('bootstrap');
 } catch (e) {}
 
-*/
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -1,5 +1,11 @@
 <template>
-    <div></div>
+    <div class="row">
+        <nav class="col-md-2 d-none d-md-block sidebar">
+            <div class="sidebar-sticky">
+                Some item
+            </div>
+        </nav>
+    </div>
 </template>
 
 <script>
@@ -7,3 +13,22 @@
 
     }
 </script>
+
+<style>
+    .sidebar {
+        position: fixed;
+        top: 0;
+        left: 0;
+        padding: 0;
+    }
+
+    .sidebar-sticky {
+        position: fixed;
+        left: 0;
+        top: 48px;
+        height: calc(100vh - 48px);
+        padding-top: 1rem;
+        overflow-x: hidden;
+        overflow-y: hidden;
+    }
+</style>
